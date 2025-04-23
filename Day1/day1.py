25d4f27db5a7cd9887b908b6626739cac7f04b44
Codingnaveen46 <naveenreddy4411@gmail.com>
2025-04-23 19:43:22 +0530

    learning python

---
+++ Day1/day1.py
@@ -0,0 +1,26 @@
+#introduction to python
+# this is the test comment in python using ctl+/
+# to add multi line comment ctrl+shift+/
+
+"""
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+"""
+
+if True:
+    print("hey this is the indentation")
+
+# understand about the indentation
+# indentation defines code block in python
+#  statements with the same identation belong to the same block
+
+# indentation is a syntax
+# identifiers are important
+# identifier should not begin with digit
+# should not contain $
+
+
+# variable name, class name, method name, package name
