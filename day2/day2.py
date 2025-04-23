9cfe541c6f1bcdfeddc72dfbeb9ec92a8785dcfd
Codingnaveen46 <naveenreddy4411@gmail.com>
2025-04-23 19:50:12 +0530

    added new day 2 file and modified day one

---
+++ day2/day2.py
@@ -0,0 +1,33 @@
+#introduction to python
+# this is the test comment in python using ctl+/
+# to add multi line comment ctrl+shift+/
+
+"""
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+to add multi line comment ctrl+shift+/
+"""
+
+if True:
+    print("hey this is the indentation")
+
+# understand about the indentation
+# indentation defines code block in python
+#  statements with the same identation belong to the same block
+
+# indentation is a syntax
+# identifiers are important
+# identifier should not begin with digit
+# should not contain $
+# Reserved keywords in python are not allowed as identifiers
+# case  sensitive
+#  _name is private identifier
+#  if name starts with __name >> strongly private identifier
+
+#  __name __ is magic method
+
+
+
+# variable name, class name, method name, package name
